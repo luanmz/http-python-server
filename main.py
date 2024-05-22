5290d887f96cb902055550890441d4cbeb0056d7
Luan Menezes <luanpereiramz@gmail.com>
2024-05-22 14:22:33 -0300

    feat: initial server setup

---
+++ main.py
@@ -0,0 +1,41 @@
+import socket
+
+HOST = "localhost"
+PORT = 5002
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((HOST, PORT))
+
+server_socket.listen(5)
+
+print(f"Listening on port: {PORT} ...")
+
+try:
+    while True:
+        client_socket, client_address = server_socket.accept()
+
+        request = client_socket.recv(1500).decode()
+        request_elements = request.split("\n")
+        request_line = request_elements[0].split()
+        http_method = request_line[0]
+        path = request_line[1]
+
+        if http_method == "GET":
+            if path == '/':
+                fin = open('index.html')
+                content = fin.read()
+                fin.close()
+                response = 'HTTP/1.1 200 OK\r\n\r\n' + content
+            else:
+                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+        else:
+            response = 'HTTP/1.1 405 Method Not Allowed\r\nAllow: GET\r\n\r\n'
+            print(response)
+
+        client_socket.sendall(response.encode())
+        client_socket.close()
+except KeyboardInterrupt:
+    print("\nServer turned off.")
+    pass
