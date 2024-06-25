c3ba7275815adec15e61651f67a41a714a582591
Luan Menezes <luanpereiramz@gmail.com>
2024-06-25 00:18:37 -0300

    feat: adding bad request scenarios and fixing latin language characters incompatibility

--- main.py
+++ main.py
@@ -16,19 +16,56 @@ try:
     while True:
 
         client_socket, client_address = server_socket.accept()
-        request = client_socket.recv(1500).decode()
 
+        try:
+            request = client_socket.recv(1500).decode()
+        except:
+            try:
+                request = client_socket.recv(1500).decode("latin-1")
+            except:
+                response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+                client_socket.sendall(response.encode())
+                client_socket.close()
+                continue
+        
+        try:
+            request_elements, request_body = request.split("\r\n\r\n", 1)
+        except:
+            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            client_socket.sendall(response.encode())
+            client_socket.close()
+            continue
+            
+        
         request_elements = request.split("\r\n")
         request_line = request_elements[0].split()
+        request_headers = request_elements[1:]
+        
+        print(request_headers, request_body)
 
         if len(request_line) < 3 or not request.__contains__("\r\n\r\n"):
             response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
         else:
             http_method = request_line[0]
             path = request_line[1]
-   
+            bad_request = False
 
-            if http_method == "GET":
+
+            headers = {}
+            for header in request_headers:
+                if ": " in header:
+                    key, value = header.split(": ", 1)
+                    headers[key] = value
+                else:
+                   bad_request == True
+                   break
+            
+            print(headers)
+                   
+            
+            if bad_request or 'Host' not in headers:
+                response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            elif http_method == "GET":
                 if path == '/':
                     fin = open('index.html')
                     content = fin.read()
